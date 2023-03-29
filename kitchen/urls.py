59096e457f10645fce814e381e56fb523aa226b6
Serhii Melnyk <serg.melnyk1998@gmail.com>
2023-03-30 01:05:42 +0300

    Add path to each view

--- kitchen/urls.py
+++ kitchen/urls.py
@@ -1,9 +1,16 @@
 from django.urls import path
 
-from kitchen.views import index
+from kitchen.views import index, DishTypeListView, DishListView, DishDetailView, CookListView, CookDetailView
 
 urlpatterns = [
     path("", index, name="index"),
+    path("dish-type/", DishTypeListView.as_view(), name="dish-type-list"),
+
+    path("dish/", DishListView.as_view(), name="dish-list"),
+    path("dish/<int:pk>/", DishDetailView.as_view(), name="dish-detail"),
+
+    path("cook/", CookListView.as_view(), name="cook-list"),
+    path("cook/<int:pk>/", CookDetailView.as_view(), name="cook-detail"),
 ]
 
 app_name = "kitchen"
