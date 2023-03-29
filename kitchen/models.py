347ed5af1327407ac04eedbc31acf457c09e93f5
Serhii Melnyk <serg.melnyk1998@gmail.com>
2023-03-29 15:18:27 +0300

    Add models.py with created models

--- kitchen/models.py
+++ kitchen/models.py
@@ -1,3 +1,49 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.urls import reverse
 
-# Create your models here.
+
+class DishType(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+    class Meta:
+        ordering = ["name"]
+
+    def __str__(self):
+        return self.name
+
+
+class Cook(AbstractUser):
+    years_of_experience = models.IntegerField(
+        blank=False,
+        error_messages={"blank": "Please, enter your years of experience."}
+    )
+
+    class Meta:
+        ordering = ["years_of_experience"]
+
+    def __str__(self):
+        return f"{self.username} ({self.first_name} {self.last_name})"
+
+    def get_absolute_url(self):
+        return reverse("kitchen:cook-detail", kwargs={"pk": self.pk})
+
+
+class Dish(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    dish_type = models.ForeignKey(
+        DishType,
+        on_delete=models.SET_NULL,
+        null=True,
+        related_name="dishes"
+    )
+    cooks = models.ManyToManyField(Cook, related_name="dishes")
+
+    class Meta:
+        ordering = ["name"]
+        verbose_name_plural = "dishes"
+
+    def __str__(self):
+        return self.name
